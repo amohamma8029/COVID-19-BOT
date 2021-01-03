0dc370d6bbb5749a23e7680928595d5fbc923ea8
amohamma8029 <89948899+amohamma8029@users.noreply.github.com>
2021-01-03 03:33:44 -0500

    added starting functions to newsAPI.py

--- api/newsAPI.py
+++ api/newsAPI.py
@@ -2,10 +2,18 @@ import asyncio
 from api import APIHandler
 from utils.asyncOperations import *
 
+#TODO: add sortBy to database
+
 class NewsAPI(APIHandler.APIHandler):
     def __init__(self):
+        self.__apiKey = 'e389c21d0c544790b26d548fd5e620d1' # make this into .env variable, double underscore added to avoid accidental re-write (constant)
         super().__init__()
 
-#TODO: put all the country codes for the newsapi in the database
-#TODO: put all the languages for the newsapi in the database
-#TODO: put all the topics for the newsapi in the database
+    async def updateSources(self, sources, language, country):
+        pass
+
+    async def getTopHeadlines(self, country, category, sources, query):
+        pass
+
+    async def getEverything(self, query, titleSearch, sources, domains, exludeDomains, fromDate, toDate, language, sortBy):
+        pass
--- database/DBHandler.py
+++ database/DBHandler.py
@@ -4,15 +4,14 @@ from motor.motor_asyncio import AsyncIOMotorClient
 import asyncio
 from utils.asyncOperations import *
 
-#TODO: Remind yourself how MongoDB works again
-#TODO: Check TNGBOT code to model your methods/functions and the class overall
 #TODO: make sure to add environment variables
 #TODO: add error handling in case database name is incorrect
 #TODO: add .env variables to dbpassword
 
 class DatabaseHandler:
-    def __init__(self, password, defaultDB=''):
-        self.cluster = AsyncIOMotorClient(f'mongodb+srv://admin:{password}@cluster0.6cb7x.mongodb.net/{defaultDB}?retryWrites=true&w=majority')
+    def __init__(self, defaultDB=''):
+        self.__password = 'kemp1935' # make this into .env variable, double underscore added to avoid accidental re-write (constant)
+        self.cluster = AsyncIOMotorClient(f'mongodb+srv://admin:{self.password}@cluster0.6cb7x.mongodb.net/{defaultDB}?retryWrites=true&w=majority')
 
     async def find(self, database, collection, search = {}):
         db = self.cluster[database]
@@ -42,12 +41,13 @@ class DatabaseHandler:
     async def update_many(self, database, collection, document, post, upsert = False):
         db = self.cluster[database]
         await db[collection].update_many(document, post, upsert)
-
+'''
 #TESTING ASYNC FUNCTIONS:
 
 post1 = {"_id":"test", "name":"corona", "food":"chicken"}
 post2 = {"_id":"rest", "name":"joe mama", "food":"bologna"}
 
 loop = asyncio.get_event_loop()
-test = loop.run_until_complete(DatabaseHandler('kemp1935').insert_many('NEWSAPI', 'articles', [post1, post2]))
+test = loop.run_until_complete(DatabaseHandler().find('NEWSAPI','miscellaneous'))
 print(test)
+'''
